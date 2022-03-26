f1574456799a790d7e86268d3bad6f894192ba49
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-03-26 19:37:07 +0100

    Reafctored ExpenseForm

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,88 +1,98 @@
-import React, { Component } from "react";
-import moment from "moment";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
-import { SingleDatePicker } from "react-dates";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { SingleDatePicker } from 'react-dates';
 
-export class ExpenseForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      description: props.expense ? props.expense.description : "",
-      note: props.expense ? props.expense.note : "",
-      amount: props.expense ? (props.expense.amount / 100).toString() : "",
-      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
-      calenderFocused: false
-    };
-  }
+const ExpenseForm = () => {
+  // SELECTORS
+  const expense = useSelector((state) => state.expenses);
 
-  onDescriptionChange = e => {
+  // STATE
+  const [description, setDescription] = useState(
+    expense ? expense.description : ''
+  );
+  const [note, setNote] = useState(expense ? expense.note : '');
+  const [amount, setAmount] = useState(
+    expense ? (expense.amount / 100).toString() : ''
+  );
+  const [createdAt, setCreatedAt] = useState(
+    expense ? moment(expense.createdAt) : moment()
+  );
+  const [calenderFocused, setCalenderFocused] = useState(false);
+
+  // HANDLERS
+  const onDescriptionChangeHandler = (e) => {
     const description = e.target.value;
-    this.setState(() => ({ description }));
+    setDescription({ description });
   };
-  onNoteChange = e => {
+
+  const onNoteChangeHandler = (e) => {
     const note = e.target.value;
-    this.setState(() => ({ note }));
+    setNote({ note });
   };
-  onAmountChange = e => {
+
+  const onAmountChangeHandler = (e) => {
     const amount = e.target.value;
-    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/))
-      this.setState(() => ({ amount }));
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) setAmount({ amount });
   };
-  onDateChange = createdAt => {
-    if (createdAt) this.setState(() => ({ createdAt }));
+
+  const onDateChangeHandler = (createdAt) => {
+    if (createdAt) setCreatedAt({ createdAt });
   };
-  onFocusChange = ({ focused }) => {
-    this.setState(() => ({ calenderFocused: focused }));
+
+  const onFocusChangeHandler = ({ focused }) => {
+    setCalenderFocused({ calenderFocused: focused });
   };
-  onSubmit = e => {
+
+  const onSubmitHandler = (e) => {
     e.preventDefault();
     this.props.onSubmit({
       description: this.state.description,
       amount: parseFloat(this.state.amount, 10) * 100,
       createdAt: this.state.createdAt.valueOf(),
-      note: this.state.note
+      note: this.state.note,
     });
   };
-  render() {
-    return (
-      <form className="form" onSubmit={this.onSubmit}>
-        <input
-          type="text"
-          placeholder="Description"
-          autoFocus
-          className="text-input"
-          value={this.state.description}
-          onChange={this.onDescriptionChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Amount"
-          className="text-input"
-          value={this.state.amount}
-          onChange={this.onAmountChange}
-          required
-        />
-        <SingleDatePicker
-          date={this.state.createdAt}
-          onDateChange={this.onDateChange}
-          focused={this.state.calenderFocused}
-          onFocusChange={this.onFocusChange}
-          numberOfMonths={1}
-          readOnly={true}
-          isOutsideRange={() => false}
-        />
-        <textarea
-          placeholder="Add a note"
-          value={this.state.note}
-          className="textarea"
-          onChange={this.onNoteChange}
-        ></textarea>
-        <button className="button">Add Expense</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form className='form' onSubmit={onSubmitHandler}>
+      <input
+        type='text'
+        placeholder='Description'
+        autoFocus
+        className='text-input'
+        value={description}
+        onChange={onDescriptionChangeHandler}
+        required
+      />
+      <input
+        type='text'
+        placeholder='Amount'
+        className='text-input'
+        value={amount}
+        onChange={onAmountChangeHandler}
+        required
+      />
+      <SingleDatePicker
+        date={createdAt}
+        onDateChange={onDateChangeHandler}
+        focused={calenderFocused}
+        onFocusChange={onFocusChangeHandler}
+        numberOfMonths={1}
+        readOnly={true}
+        isOutsideRange={() => false}
+      />
+      <textarea
+        placeholder='Add a note'
+        value={note}
+        className='textarea'
+        onChange={onNoteChangeHandler}
+      ></textarea>
+      <button className='button'>Add Expense</button>
+    </form>
+  );
+};
 
 export default ExpenseForm;

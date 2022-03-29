26f1b31ae46a470f4c6c17150d500fdd912eb85a
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-03-29 17:27:59 +0100

    test case for remove expense if id not found

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -19,3 +19,12 @@ test('should remove expense by id', () => {
   const state = expensesReducer(expenses, action);
   expect(state).toEqual([expenses[0], expenses[2]]);
 });
+
+test('should not remove expenses if id not found', () => {
+  const action = {
+    type: REMOVE_EXPENSE,
+    payload: -1,
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[1], expenses[2]]);
+});

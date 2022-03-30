7a17a5ecee3569cabaa979e49173ec53c82650ca
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-03-30 17:40:28 +0100

    Fixed NotFoundPage tex case

--- src/tests/components/NotFoundPage.test.js
+++ src/tests/components/NotFoundPage.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
+import NotFoundPage from '../../components/NotFoundPage';
 
 test('should render NotFoundPage correctly', () => {
-  const wrapper = shallow(
-    <div>
-      404! - <Link to='/'>Go home</Link>
-    </div>
-  );
+  const wrapper = shallow(<NotFoundPage />);
   expect(wrapper).toMatchSnapshot();
 });

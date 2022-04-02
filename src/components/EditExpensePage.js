b2bb86d49a86fb06402fabd3f7e5ea9c9cb5702f
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-04-02 21:58:36 +0100

    corrected test case to  render EditExpensePage

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -7,24 +7,26 @@ const EditExpensePage = ({ history, match }) => {
   const dispatch = useDispatch();
 
   // SELECTORS
-  const expense = useSelector((state) =>
-    state.expenses.find((expense) => expense.id === match.params.id)
+  const expenses = useSelector((state) => state.expenses);
+
+  const expenseToEdit = expenses.find(
+    (expense) => expense.id === match.params.id
   );
 
   // HANDLERS
   const onSubmitHandler = (updatedExpense) => {
-    dispatch(editExpense(expense.id, updatedExpense));
+    dispatch(editExpense(expenseToEdit.id, updatedExpense));
     history.push('/');
   };
 
   const onRemoveHandler = () => {
-    dispatch(removeExpense(expense.id));
+    dispatch(removeExpense(expenseToEdit.id));
     history.push('/');
   };
 
   return (
     <div>
-      <ExpenseForm expense={expense} onSubmit={onSubmitHandler} />
+      <ExpenseForm expense={expenseToEdit} onSubmit={onSubmitHandler} />
       <button onClick={onRemoveHandler}>Remove</button>
     </div>
   );
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import * as reactRedux from 'react-redux';
 import EditExpensePage from '../../components/EditExpensePage';
+import expenses from '../fixtures/expenses';
 
 jest.mock('react-redux', () => ({
   useDispatch: jest.fn(),
   useSelector: jest.fn(),
 }));
 
-test('should render EditExpensePage', () => {
-  const history = { push: jest.fn() };
-  const match = { params: { id: 1 } };
-  const wrapper = shallow(<EditExpensePage history={history} match={match} />);
-  expect(wrapper).toMatchSnapshot();
+describe('edit expense actions', () => {
+  beforeEach(() => {
+    useSelectorMock.mockImplementation((selector) => selector(mockStore));
+  });
+
+  afterEach(() => {
+    useSelectorMock.mockClear();
+  });
+
+  const useSelectorMock = reactRedux.useSelector;
+
+  const mockStore = {
+    expenses,
+  };
+
+  test('should render EditExpensePage with expense data', () => {
+    const history = { push: jest.fn() };
+    const match = { params: { id: '1' } };
+    const wrapper = shallow(
+      <EditExpensePage history={history} match={match} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should handle editExpense', () => {});
 });

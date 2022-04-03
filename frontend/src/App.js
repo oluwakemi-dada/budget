def1de67d4d053410cdeae1abc314a340a8cd830
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-04-03 15:46:26 +0100

    Setup production build and server

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
+import './styles/styles.scss';
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+export default App;

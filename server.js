1a94fdbd0f44caea10396ed59bb65b48d38eba4d
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-04-03 16:45:20 +0100

    updates to server

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const express = require('express');
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 const publicPath = path.join(__dirname, 'frontend', 'build');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+// LOAD BUILD INDEX.JS FILE
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.listen(port, () => {
   console.log('server is up!');

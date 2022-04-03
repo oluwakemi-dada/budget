ab5139ccedad081d17c40b8d7a1b0dfbcddf1b53
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-04-03 15:58:29 +0100

    Setup production build and server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '/frontend/build');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 });
 
 app.listen(port, () => {
